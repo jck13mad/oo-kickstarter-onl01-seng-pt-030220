730b403aacf1db15a880f0c122ba38643c62f086
Jack Madden <jck13mad@gmail.com>
2020-04-02 22:02:50 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,10 +5,12 @@ class Backer
  
   def initialize(backed_projects)
     @backed_projects = []
+    @name = name
   end
    
   def back_project(project)
     @backed_projects << project
+    self.
   end
   
  
