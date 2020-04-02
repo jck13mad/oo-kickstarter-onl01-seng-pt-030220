2007f88ee1b563fb352bb1d7a35b0298c540f27d
Jack Madden <jck13mad@gmail.com>
2020-04-02 22:04:14 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,8 +3,9 @@ class Project
   attr_reader :title 
   attr_accessor :backers
   
-  def initialize(backers)
+  def initialize(title)
     @backers = []
+    @title = title
   end
   
   def add_backer(backer)
